8bc9c5cf74da9e3fafd5ff9851cee0c9bf045f65
nawal abdualghani <nawalabdualghani@gmail.com>
2019-11-20 08:25:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 
 // Write your code here
 
-let num1 ;
-let num2 ;
-function multiply(){
-  num1=2;
-  num2=31;
-  let multiply=num1*num2;
-  console.log(multiply);
-}
+let num1 =2;
+let num2 =31;
+let multiply=num1*num2;
+
 let random = Math.floor( Math.random() * 1000 ) + 1
 
 let mod = 16 % 6
